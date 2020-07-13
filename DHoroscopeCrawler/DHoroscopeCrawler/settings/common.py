414195269216a466cdd40c5306dff9287883d23b
Pranjal <pranjal@smalldaytech.com>
2020-07-13 16:09:51 +0530

    Commented base_dir

--- DHoroscopeCrawler/DHoroscopeCrawler/settings/common.py
+++ DHoroscopeCrawler/DHoroscopeCrawler/settings/common.py
@@ -113,7 +113,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-print(BASE_DIR)
+#print(BASE_DIR)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '../main/static')
